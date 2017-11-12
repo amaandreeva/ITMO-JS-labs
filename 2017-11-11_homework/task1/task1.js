f21af2ae3cf2c7a2de4aa703d56c7870c12be289
andreevaam <and.anastasiia@gmail.com>
2017-11-13 00:29:43 +0300

    2017-11-11_homework

---
+++ 2017-11-11_homework/task1/task1.js
@@ -0,0 +1,4 @@
+var a = Number(prompt("Введите первое число"));
+var b = Number(prompt("Введите второе число"));
+var c = Number(prompt("Введите третье число"));
+alert("Наименьшее число: " + Math.min(a, b, c));
