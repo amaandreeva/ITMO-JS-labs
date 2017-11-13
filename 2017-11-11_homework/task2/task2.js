4dd78e05df9150bf99217ef212d78c81bada10d7
andreevaam <and.anastasiia@gmail.com>
2017-11-13 12:26:39 +0300

    2017-11-11_homework

--- 2017-11-11_homework/task2/task2.js
+++ 2017-11-11_homework/task2/task2.js
@@ -1,18 +1,19 @@
 var anyNumber = Number(prompt("Выберете любое целое число, лежащее в диапазоне от -999 до 999 и увидите словесное описание данного числа! =)"));
-if (anyNumber === parseInt(anyNumber, 10) && anyNumber >= -999 && anyNumber <= -100) {
-        alert("Это отрицательное трехзначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber >= -99 && anyNumber <= -10) {
-    alert("Это отрицательное двухзначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber >= -9 && anyNumber <= -1) {
-    alert("Это отрицательное однозначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber >= 1 && anyNumber <= 9) {
-    alert("Это положительное однозначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber >= 10 && anyNumber <= 99) {
-    alert("Это положительное двузначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber>= 100 && anyNumber <= 999) {
-    alert("Это положительное трехзначное число.");
-} else if (anyNumber === parseInt(anyNumber, 10) && anyNumber === 0) {
-    alert("Это нулевое число.");
-} else {
+
+if (anyNumber != parseInt(anyNumber, 10) || anyNumber < -999 || anyNumber > 999) {
     alert("Упс... Вы ввели нецелое число или число вне диапазона от -999 до 999 =(");
-}
+} else if (anyNumber >= -999 && anyNumber <= -100) {
+    alert("Это отрицательное трехзначное число.");
+} else if (anyNumber >= -99 && anyNumber <= -10) {
+    alert("Это отрицательное двухзначное число.");
+} else if (anyNumber >= -9 && anyNumber <= -1) {
+    alert("Это отрицательное однозначное число.");
+} else if (anyNumber >= 1 && anyNumber <= 9) {
+    alert("Это положительное однозначное число.");
+} else if (anyNumber >= 10 && anyNumber <= 99) {
+    alert("Это положительное двузначное число.");
+} else if (anyNumber >= 100 && anyNumber <= 999) {
+    alert("Это положительное трехзначное число.");
+} else {
+    alert("Это нулевое число.");
+} 
